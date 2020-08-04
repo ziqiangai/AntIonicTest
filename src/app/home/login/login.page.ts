a8d71b91eb695e3c61aeaabbb792c0d4731046e9
1694392889@qq.com <666888aaaQQQ[]>
2020-08-04 09:16:09 +0800

    登录界面

---
+++ src/app/home/login/login.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  form = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    remember_me: [true]
+  });
+  constructor(private fb: FormBuilder,
+              private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  submitForm(value: {}) {
+    console.log(value);
+    this.router.navigate(['./home'], {replaceUrl: true});
+    console.log('login');
+  }
+}
