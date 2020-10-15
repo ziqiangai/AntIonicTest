a4783d83e732820da57764bab6ce5bc4c8d3bb48
Exclamation-mark / tool <1694392889@qq.com>
2020-10-15 13:58:23 +0800

    修改主界面后退按钮判断退出的条件

--- src/app/home/login/login.page.ts
+++ src/app/home/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+import {FormBuilder, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
